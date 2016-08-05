3dbc667cae74e979d85780b4e7ef5f311c922e24
Rohith R <rohithr31@Rohiths-MacBook-Air.local>
2016-08-05 09:43:04 +0800

    Added full contest testing

---
+++ fetch_contest_testcases.py
@@ -0,0 +1,26 @@
+from bs4 import BeautifulSoup
+import urllib
+import string
+import retrive_test_cases
+
+def fetch_contest_testcases (contest_url) :
+	response = urllib.urlopen (contest_url)
+	html = response.read ()
+
+	soup = BeautifulSoup(html, 'html.parser')
+
+	problems = soup.findAll ("select",{"name":"submittedProblemIndex"})
+
+	number_of_problems = 0
+	for ele in problems[0] :
+		if ele != '\n' :
+			number_of_problems += 1
+
+	number_of_problems -= 1
+
+	problem_names = list(string.ascii_uppercase)
+
+	for i in xrange(number_of_problems) :
+		retrive_test_cases.retrive_test_cases(contest_url+"/problem/"+str(problem_names[i]))
+
+# fetch_contest_testcases ("http://codeforces.com/contest/703")
