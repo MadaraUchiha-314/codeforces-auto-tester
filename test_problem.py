114cb181b9d7a0b3334c50482c194cfbad3177af
Rohith R <rohithr31@Rohiths-MacBook-Air.local>
2016-08-05 16:39:30 +0800

    Removing all files

--- test_problem.py
+++
@@ -1,9 +0,0 @@
-import os
-import test_problem_for_testcase
-
-
-def test_problem (problem_name,lang="cpp",typ="normal"):
-	number_of_tests = len([name for name in os.listdir(problem_name+"/input") if os.path.isfile(problem_name+"/input/"+name)])
-
-	for i in xrange (number_of_tests):
-		test_problem_for_testcase.test_problem_for_testcase(problem_name,lang,str(i),typ)
